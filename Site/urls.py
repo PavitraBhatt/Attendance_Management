6ffa9cc82ee48a039c54d29d471c2d87e0329437
VaibhavRawal30 <148911270+VaibhavRawal30@users.noreply.github.com>
2023-11-06 18:37:02 +0530

    urls.py

--- Site/urls.py
+++ Site/urls.py
@@ -1,14 +1,23 @@
 from . import views
 from django.urls import path
+from .views import SiteGetView,SiteGetIdView,SiteUpdateView,SitePostView,SiteDeleteView,SiteDetailView,SiteCreateView
 
 urlpatterns = [
     path('site/',views.site_list),
-    path('site/<str:pk>',views.site_detail),
-    path('sitecreate/',views.site_create),
-    path('getsite/',views.site_get),
-    path('getsite/<str:SiteID>/',views.site_get_id),
-    path('postsite/',views.site_post),
+    # path('site/<str:pk>',views.site_detail),
+    path('api/site/detail/<int:pk>/', SiteDetailView.as_view(), name='site_detail'),
+    # path('sitecreate/',views.site_create),
+    path('api/site/create/', SiteCreateView.as_view(), name='site_create'),
+    # path('getsite/',views.site_get),
+    # path('getsite/<str:SiteID>/',views.site_get_id),
+    # path('postsite/',views.site_post),
     # path('updatesite/',views.site_put),
-    path('updatesite/<str:SiteID>/', views.site_update, name='site-update'),
-    path('deletesite/',views.site_delete),
+    # path('updatesite/<str:SiteID>/', views.site_update, name='site-update'),
+    # path('deletesite/',views.site_delete),
+    path('site_get/', SiteGetView.as_view(), name='site-get'),
+    path('api/site/<str:SiteID>/', SiteGetIdView.as_view(), name='site_get_id'),
+    path('api/site_update/<str:SiteID>/', SiteUpdateView.as_view(), name='site_update'),
+    path('api/site/post/', SitePostView.as_view(), name='site_post'),
+    path('api/site/delete/', SiteDeleteView.as_view(), name='site_delete'),
 ]
+ 
