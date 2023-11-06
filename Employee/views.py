f231afa5ce7d826a6f6ab295d907ddd817370d10
VaibhavRawal30 <148911270+VaibhavRawal30@users.noreply.github.com>
2023-11-06 18:15:09 +0530

    views.py

--- Employee/views.py
+++ Employee/views.py
@@ -15,6 +15,8 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.csrf import csrf_exempt
 import json
 from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 
@@ -34,140 +36,87 @@ def employee_list(request):
     # return HttpResponse(json_data,content_type='application/json')
     return JsonResponse(serializer.data,safe=False)
 
-@csrf_exempt
-def employee_create(request):
-    if request.method == 'POST':
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        pythondata = JSONParser().parse(stream)
-        serializer = EmployeeSerializer(data = pythondata)
+
+class EmployeeCreateView(APIView):
+    def post(self, request):
+        json_data = request.data
+        serializer = EmployeeSerializer(data=json_data)
         if serializer.is_valid():
             serializer.save()
-            res={'msg':'Employee Created Succesfully !'}
-            json_data = JSONRenderer().render(res)
-            return HttpResponse(json_data,content_type='application/json')
-        
-        else :
-            json_data = JSONRenderer().render(serializer.errors)
-            return HttpResponse(json_data,content_type= 'application/json')
+            return Response({'msg': 'Employee Created Successfully!'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-@csrf_exempt       
-def employee_get(request): 
-    
-    if request.method == 'GET':
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        # pythondata = JSONParser().parse(stream)
-        decoded_data = stream.getvalue().decode('utf-8')
-        pythondata = json.dumps(decoded_data)
-        # id = pythondata.get('id',None)
-        id = pythondata.get('id', None) if isinstance(pythondata, dict) else None
-
+     
+class EmployeeGetView(APIView):
+    def get(self, request):
+        id = request.query_params.get('id')
         if id is not None:
-            site = Employee.objects.get(id=id)
-            serializer = EmployeeSerializer(site)
-            json_data = JSONRenderer().render(serializer.data)
-            return HttpResponse(json_data,content_type ='application/json')
-        emp = Employee.objects.all()
-        serializer = EmployeeSerializer(emp,many=True)
-        json_data = JSONRenderer().render(serializer.data)
-        return HttpResponse(json_data,content_type ='application/json')
+            try:
+                emp = Employee.objects.get(id=id)
+                serializer = EmployeeSerializer(emp)
+                return Response(serializer.data)
+            except Employee.DoesNotExist:
+                return Response({'error': 'Employee not found'}, status=status.HTTP_404_NOT_FOUND)
+        else:
+            emp = Employee.objects.all()
+            serializer = EmployeeSerializer(emp, many=True)
+            return Response(serializer.data)
 
-@csrf_exempt
-def employee_get_id(request, EmployeeID=None):  # Accept the 'EmployeeID' parameter
-    
-    if request.method == 'GET':
+
+class EmployeeGetIdView(APIView):
+    def get(self, request, EmployeeID=None):
         if EmployeeID is not None:
             try:
                 emp_obj = Employee.objects.get(EmployeeID=EmployeeID)  # Assuming 'EmployeeID' is the field name
                 serializer = EmployeeSerializer(emp_obj)
-                json_data = JSONRenderer().render(serializer.data)
-                return HttpResponse(json_data, content_type='application/json')
+                return Response(serializer.data)
             except Employee.DoesNotExist:
-                return HttpResponse(status=404)
-        
+                return Response(status=status.HTTP_404_NOT_FOUND)
+
         emp = Employee.objects.all()
         serializer = EmployeeSerializer(emp, many=True)
-        json_data = JSONRenderer().render(serializer.data)
-        return HttpResponse(json_data, content_type='application/json')
+        return Response(serializer.data)
     
-@csrf_exempt
-def employee_post(request):
-    if request.method == 'POST':
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        pythondata = JSONParser().parse(stream)
-        serializer =EmployeeSerializer(data = pythondata)
+class EmployeePostView(APIView):
+    def post(self, request):
+        json_data = request.data
+        serializer = EmployeeSerializer(data=json_data)
         if serializer.is_valid():
             serializer.save()
-            res = {'msg':'Employee Created Succesfully !'}
-            json_data = JSONRenderer().render(res)
-            return HttpResponse(json_data,content_type = 'application/json', status = status.HTTP_201_CREATED)  
-        else :
-            json_data = JSONRenderer().render(serializer.errors)
-            return HttpResponse(json_data,content_type = 'application/json')
+            res = {'msg': 'Employee Created Successfully!'}
+            return Response(res, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
-# @csrf_exempt
-# def employee_put(request):
-#     if request.method == 'PUT':
-#         json_data = request.body
-#         stream = io.BytesIO(json_data)
-#         pythondata = JSONParser().parse(stream)
-#         employee_id = pythondata.get('EmployeeID')
-#         # emp = Employee.objects.filter(EmployeeID=employee_id)
-#         # serializer = EmployeeSerializer(employee,data = pythondata, partial = True) #if partial =False you write then we have give full information fully update
-#         # if serializer.is_valid():
-#         #     serializer.save()
-#         #     res = {'msg':'data updated'}
-#         #     json_data = JSONRenderer().render(res)
-#         #     return HttpResponse(json_data,content_type = 'application/json')
-        
-#         # else:
-#         #     json_data = JSONRenderer().render(serializer.errors)
-#         #     return HttpResponse(json_data,content_type = 'application/json')
-#         try:
-#             emp = Employee.objects.filter(EmployeeID=employee_id)
-#             if emp.exists():
-#                 # Update the attributes of each site in the queryset
-#                 for emp in emp:
-#                     serializer = EmployeeSerializer(emp, data=pythondata, partial=True)
-#                     if serializer.is_valid():
-#                         serializer.save()
-#                 response_data = {'msg': 'Data updated'}
-#             else:
-#                 response_data = {'error': 'No employee found with the given EmployeeID'}
-#         except :
-#             response_data = {'error': 'Employee does not exist'}
-        
-#         return JsonResponse(response_data)
+class EmployeeUpdateView(APIView):
+    def get_object(self, EmployeeID):
+        try:
+            return Employee.objects.get(EmployeeID=EmployeeID)
+        except Employee.DoesNotExist:
+            return None
 
-@csrf_exempt
-def employee_update(request, EmployeeID):
-    try:
-        emp_obj = Employee.objects.get(EmployeeID=EmployeeID)
-    except Employee.DoesNotExist:
-        return HttpResponse(status=404)
+    def post(self, request, EmployeeID):
+        emp_obj = self.get_object(EmployeeID)
+        if emp_obj is None:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = EmployeeSerializer(emp_obj, data=data)
+        serializer = EmployeeSerializer(emp_obj, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-@csrf_exempt
-def employee_put(request):
-    if request.method == 'PUT':
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        pythondata = JSONParser().parse(stream)
-     
-        if isinstance(pythondata, list):
+
+class EmployeePutView(APIView):
+    def put(self, request):
+        json_data = request.data
+
+        if isinstance(json_data, list):
             # Handle the case of updating multiple employees
             response_data = {'msg': 'Updating multiple employees'}
-            for data in pythondata:
+            for data in json_data:
                 employee_id = data.get('EmployeeID')
                 try:
                     emp = Employee.objects.filter(EmployeeID=employee_id)
@@ -184,12 +133,12 @@ def employee_put(request):
                     response_data = {'error': str(e)}
         else:
             # Handle the case of updating a single employee
-            employee_id = pythondata.get('EmployeeID')
+            employee_id = json_data.get('EmployeeID')
             try:
                 emp = Employee.objects.filter(EmployeeID=employee_id)
                 if emp.exists():
                     for emp_obj in emp:
-                        serializer = EmployeeSerializer(emp_obj, data=pythondata, partial=True)
+                        serializer = EmployeeSerializer(emp_obj, data=json_data, partial=True)
                         if serializer.is_valid():
                             serializer.save()
                         else:
@@ -199,24 +148,19 @@ def employee_put(request):
             except Exception as e:
                 response_data = {'error': str(e)}
         
-        return JsonResponse(response_data)
+        return Response(response_data)
 
-@csrf_exempt
-def employee_delete(request):       
-    if request.method == 'DELETE':
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        pythondata = JSONParser().parse(stream)
-        EmployeeID = pythondata.get('EmployeeID')
-        # emp = Employee.objects.get(EmployeeID=EmployeeID)
-        # emp.delete()
-        # res={'msg':'Data Deleted'}
+
+class EmployeeDeleteView(APIView):
+    def delete(self, request):
+        json_data = request.data
+        EmployeeID = json_data.get('EmployeeID')
+
         try:
             emp = Employee.objects.get(EmployeeID=EmployeeID)
             emp.delete()
-            res = {'msg': f"Employee is deleted with the EmployeeID : {EmployeeID}"}
-        except :
+            res = {'msg': f"Employee is deleted with the EmployeeID: {EmployeeID}"}
+            return Response(res, status=status.HTTP_204_NO_CONTENT)
+        except Employee.DoesNotExist:
             res = {'error': 'Employee not found'}
-        # json_data = JSONRenderer().render(res)
-        # return HttpResponse(json_data,content_type = 'application/json')
-        return JsonResponse(res,safe=False,status = status.HTTP_204_NO_CONTENT)
+            return Response(res, status=status.HTTP_404_NOT_FOUND)
