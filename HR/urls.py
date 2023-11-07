319c20bab7d3283b38924bde8ca7d239d5acd2ce
PraneshPatel3103 <149478604+PraneshPatel3103@users.noreply.github.com>
2023-11-07 17:15:57 +0530

    Update urls.py

--- HR/urls.py
+++ HR/urls.py
@@ -12,18 +12,6 @@ from .views import HRUpdateView
 from .views import HRDeleteView
 
 
-'''urlpatterns = [
-    path('hr/',views.hr_list),
-    path('hr/<str:pk>',views.hr_detail),
-    path('hrcreate/',views.hr_create),
-    path('gethr/<str:HRID>/',views.hr_get_id),
-    path('gethr/',views.hr_get),
-    path('posthr/',views.hr_post),
-    path('updatehr/<str:HRID>/', views.hr_update, name='hr-update'),
-    path('updatehr/',views.hr_put),
-    path('deletehr/',views.hr_delete), 
-]'''
-
 
 
 urlpatterns = [
